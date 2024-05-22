9d2d3b9bdce9d52d6c6c7f9d0fc0264491d46d3d
juanjo <juanjo.sales.sc@gmail.com>
2024-05-22 10:59:06 +0200

    klzsnbóaskldb

--- src/a/prueba.java
+++ src/a/prueba.java
@@ -5,7 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("hola tus muertos");
 		
 		System.out.println("hola");
 
